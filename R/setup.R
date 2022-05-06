70269cb54c8fed92d64bc52001b7510b86925605
Nelson Johansen <njjohansen@ucdavis.edu>
2022-05-06 10:35:17 -0700

    Add setup.R module

---
+++ R/setup.R
@@ -0,0 +1,16 @@
+#' Setup ArchR for genome and threads
+#'
+#' Sets global variables about specific genome build and number of threads to utilze on the machine.
+#'
+#' @param genome Valid genome name for ArchR: mm10, hg38
+#' @param archr.threads Number of threads to utilize, warning increases memory usage as well.
+#'
+#' @return None
+#'
+#' @export
+archr_setup = function(genome, archr.threads){
+    ## Genome annotation info using ArchR defaults
+    addArchRGenome(genome)
+    ## Multi-core
+    addArchRThreads(threads = archr.threads) 
+}
