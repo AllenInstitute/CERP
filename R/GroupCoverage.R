ff8b320f02f037d6076473c8c994491ac90c1606
marcusjhooper <36462147+marcusjhooper@users.noreply.github.com>
2022-05-06 11:32:12 -0700

    Added GroupCoverage and PeakCalling
    Added GroupCoverage and PeakCalling

---
+++ R/GroupCoverage.R
@@ -0,0 +1,11 @@
+#' Creates coverages for a variable of interest  
+#'
+#' 
+#'
+#' @param ArchR_project ArchR project 
+#' 
+#'
+#' @return ArchR_project with coverages calculated for variable of interest
+#'
+#' @export
+
---
+++ R/PeakCalling.R
@@ -0,0 +1,22 @@
+#' Calls peaks downstream of GroupCoverage based on variable of interest
+#'
+#' 
+#'
+#' @param ArchR_project ArchR project with group coverages
+#' @param cell_population cell population of interest this should be in cellColData
+#'
+#' @return ArchR_project with peaks called
+#'
+#' @export
+
+PeakCalling<-function(ArchR_project = NULL,cell_population = NULL){
+  dir.create(output_folder)
+ 
+  print(paste("Calling peaks by",cell_population))
+  ArchR_project <- addReproduciblePeakSet(ArchRProj = ArchR_project, groupBy = cell_population, peakMethod = "Macs2",minCells = 50,force = T)
+  print("Peak calling finished")
+  
+  
+  return(ArchR_project)
+  
+}
