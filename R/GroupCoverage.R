7dee270bd40aed43a2839c7c3058315c05b01757
marcusjhooper <36462147+marcusjhooper@users.noreply.github.com>
2022-05-11 13:48:53 -0700

    Update GroupCoverage.R
    added group coverage function

--- R/GroupCoverage.R
+++ R/GroupCoverage.R
@@ -8,4 +8,27 @@
 #' @return ArchR_project with coverages calculated for variable of interest
 #'
 #' @export
-
+GroupCoverage<-function(archr.proj = NULL, groupBy = NULL, ...){
+  
+  print(paste("Creating Group Coverages by: ", groupBy))
+  
+  archr.proj <- addGroupCoverages(
+    ArchRProj = archr.proj,
+    groupBy = groupBy,
+    useLabels = TRUE,
+    minCells = 50,
+    maxCells = 10000,
+    maxFragments = 25 * 10^6,
+    minReplicates = 2,
+    maxReplicates = 5,
+    sampleRatio = 0.8,
+    kmerLength = 6,
+    threads = getArchRThreads(),
+    returnGroups = FALSE,
+    parallelParam = NULL,
+    force = TRUE,
+    verbose = TRUE,
+    logFile = createLogFile("addGroupCoverages")
+  )
+  return(archr.proj)
+}
