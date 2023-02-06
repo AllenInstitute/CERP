0271ed55126b669379e1f674cff7e901dc583b8c
marcusjhooper <36462147+marcusjhooper@users.noreply.github.com>
2023-02-06 15:17:14 -0800

    Update peak_to_gene.R

--- R/peak_to_gene.R
+++ R/peak_to_gene.R
@@ -1,5 +1,5 @@
 #'gets peak to gene links and merge with existing marker peaks'
-
+# archr.proj must have ar_id and ar_directory for this to work properly. Future updates will allow for counts matrices to generate gene expression matrices.
 #' @param archr.proj 
 #' @param max_distance distance from the gene to search for linked peaks - default 250kb
 #' @param reduced_dims reduced dimension parameter for p2gene links ArchR function, default "LSI_Combined"
