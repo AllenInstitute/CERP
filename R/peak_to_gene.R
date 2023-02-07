ca255a6baf5322e9c1ec6e6eddde96ff2cea4d94
marcusjhooper <36462147+marcusjhooper@users.noreply.github.com>
2023-02-06 16:30:20 -0800

    Version 1
    need Gene expression matrix and LSI combined for version 1

--- R/peak_to_gene.R
+++ R/peak_to_gene.R
@@ -6,3 +6,25 @@
 #' @param useMatrix' matrix to use for constructing p2gene links, can be GeneExpressionMatrix (default) or GeneIntegrationMatrix
 #' @param marker.file' annotated marker peaks file path
 
+peak2Gene = function(archr.proj = NULL, max_distance = 250000, reduced_dims = "LSI_Combined", useMatrix = "GeneExpressionMatrix",
+                     marker.file = file.path(file.path(getOutputDirectory(archr.proj), "MarkerPeaks", groupBy), paste0(groupBy, "_annotated_markerPeaks.tsv"))){
+  #library(magrittr)
+  #load markerPeaks
+  #check for LSI if not there, run LSI 
+  archr.proj <- addPeak2GeneLinks(
+    ArchRProj = archr.proj,
+    reducedDims = reduced_dims,
+    useMatrix = useMatrix,
+    maxDist = max_distance,
+  )
+  p2geneDF <- metadata(archr.proj@peakSet)$Peak2GeneLinks
+  p2geneDF$correlated_gene <- mcols(metadata(p2geneDF)$geneSet)$name[p2geneDF$idxRNA]
+  p2geneDF$coordinates <- (metadata(p2geneDF)$peakSet %>% {paste0(seqnames(.), ":", start(.), "-", end(.))})[p2geneDF$idxATAC]
+  p2geneDF$cor_FDR<-p2geneDF$FDR
+  p2geneDF = as.data.table(p2geneDF)
+  p2geneDF = p2geneDF[,.(coordinates,correlated_gene,Correlation,cor_FDR)]
+  marker.table = as.data.table(read.table(marker.file, sep = "\t",header = T))
+  marker.table = merge.data.table(p2geneDF,marker.table, by = "coordinates", all.x = TRUE)
+  marker.table = marker.table[!is.na(peak.id) & !is.na(Correlation),]
+  write.table(marker.table,file = marker.file, sep = "\t")
+}
