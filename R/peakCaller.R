8ef913579ba201a0e11df27af1b837ee45a9c9ba
UCDNJJ <njjohansen@ucdavis.edu>
2023-03-29 14:21:16 -0700

    syntax, style and documentation

--- R/peakCaller.R
+++ R/peakCaller.R
@@ -1,115 +1,141 @@
+#' Calls peaks currently utilizing ArchR
+#'
+#' Sets global variables about specific genome build and number of threads to utilze on the machine.
+#'
+#' @param archr.proj An already setup ArchR project or a name (string) to build a new ArchR project. If passing an already defined archr.proj then arrow.file.dir and cell.annotation.file will not be used.
+#' @param archr.genome Valid genome name for ArchR: mm10, hg38
+#' @param groupBy Metadata field to group cells by
+#' @param dataset Information about the dataset that these marker peaks belong to. E.g. Brain region, etc.
+#' @param archr.threads Number of threads to utilize, warning increases memory usage as well.
+#' @param arrow.file.dir Directory where all the Arrow files are stored 
+#' @param cell.annotation.file File that contains sample_id and additional cell annotations to append to the ArchRProject
+#' @param archr.clustering Boolean (T/F) determining if clustering/LSI should be run.
+#' @param varFeatures Number of variable features to use for cluster
+#' @param resolution Vector of clustering resolutions for multi-round clusters
+#' @param tileSize Width of tiles that will section the genome
+#' @param normMethod Normalization method for fragment pileups 
+#' @param maxCells Number of cells to use for each group.
+#' @param archr.visualize Should marker peak visualizations be produced
+#' @param output.folder If supplied we will save the marker peak table and ArchRProject to this location.
+#' @param publish Set to 'TRUE' if you are ready to make the marker peak table viewable on Shiny.
+#' @param ucsc.user A ucsc genome browser user/account which to contains the session to build links from.
+#' @param ucsc.session A registred session for the UCSC genome browser user/account specified in `ucsc.user`.
+#' @param calculate_gini_index ADD INFO
+#' @param calculate_peak2gene ADD INFO
+#' @param max_distance ADD INFO
+#' 
+#' @return ArchRProject, markerPeaks
+#'
+#' @export
 peakCaller = function(archr.proj, archr.genome, groupBy, dataset, archr.threads=4,               
                       arrow.file.dir=NULL, cell.annotation.file=NULL,
                       archr.clustering=FALSE, varFeatures=15000, resolution=c(0.2,1,2), 
                       tileSize=25, normMethod="ReadsInTSS", maxCells=NULL,              
-                      archr.visualize=FALSE, output.folder=NULL, publish=FALSE, ucsc.user=NULL, ucsc.session=NULL,
-                      calculate_p2gene = FALSE, samp.dat = NULL,max_distance = 250000
-){
-  
-  ## Error handling
-  archr.proj = tryCatch({
-    
-    ## Run basic checks before getting to far.
-    #.run_checks()
-    
-    ## unique_peak_calling_id
-    start_time = Sys.time()
-    unique.id = gsub(start_time, pattern = " |PDT|-|:", replacement = "")
-    print(paste0("unique.id for this run:", unique.id))
-    
-    ## Remove old directory and create new directory under ArchR project for marker peak results
-    print("creating marker_peaks directory")
-    #unlink(file.path(getOutputDirectory(archr.proj), "MarkerPeaks", groupBy))
-    dir.create(file.path(getOutputDirectory(archr.proj), "MarkerPeaks", groupBy), showWarnings = TRUE, recursive=TRUE)
-    #dir.create(file.path(getOutputDirectory(archr.proj), "MarkerPeaks"), showWarnings = TRUE)
-    #dir.create(file.path(getOutputDirectory(archr.proj), "MarkerPeaks", groupBy), showWarnings = TRUE)
-    
-    ##
-    print("Running initial ArchR setup")
-    setup(archr.genome = archr.genome,
-          archr.threads = archr.threads)
-    
-    ##
-    print("Building/checking ArchRProject")
-    archr.proj = build(arrow.file.dir = arrow.file.dir, 
-                       cell.annotation.file = cell.annotation.file, 
-                       archr.proj = archr.proj)
-    
-    ##
-    if(archr.clustering == TRUE){
-      print("Performing clustering and LSI")
-      archr.proj = clusterDimReduc(archr.proj = archr.proj, 
-                                   varFeatures = varFeatures, 
-                                   resolution = resolution)
-    }
-    
-    ##
-    print("Defining group coverages")
-    addArchRThreads(1)
-    archr.proj = groupCoverage(archr.proj = archr.proj,
-                               groupBy = groupBy)
-    
-    ##
-    print("Calling peaks")
-    addArchRThreads(archr.threads)
-    archr.proj = peakCalling(archr.proj = archr.proj,
-                             groupBy = groupBy)
-    
-    ##
-    print("Identifying marker peaks")
-    addArchRThreads(archr.threads)
-    archr.proj = markerPeaks(archr.proj = archr.proj,
-                             groupBy = groupBy, 
-                             unique.id = unique.id)
-    
+                      archr.visualize=FALSE, output.folder=NULL, publish=NULL, ucsc.user=NULL, ucsc.session=NULL,
+                      calculate_gini_index = FALSE, calculate_peak2gene = FALSE,
+                      max_distance = 250000, ){
 
-    ## Record marker table location 
-    archr.proj@projectMetadata[[paste0("loc_marker_table_", groupBy)]] = file.path(getOutputDirectory(archr.proj), "MarkerPeaks", groupBy, paste0(groupBy, "_markerPeaks.tsv"))
-    
-    ##
-    if(!is.null(archr.proj@projectMetadata[[paste0(groupBy, "_markerPeaks")]])){
-      print("Loading from project")
-      marker.table = archr.proj@projectMetadata[[paste0(groupBy, "_markerPeaks")]]
-    }else{
-      print("Loading from csv")
-      marker.table = read.csv(archr.proj@projectMetadata[[paste0("loc_marker_table_", groupBy)]])
-    }
-    
-    ##
-    annotatePeaks(marker.table = marker.table, 
-                  archr.proj = archr.proj, 
-                  groupBy = groupBy, 
-                  dataset = dataset,
-                  publish = publish,
-                  filename = file.path(file.path(getOutputDirectory(archr.proj), "MarkerPeaks", groupBy), paste0(groupBy, "_annotated_markerPeaks.tsv")),
-                  ucsc.user = ucsc.user,
-                  ucsc.session = ucsc.session)
-    #p2gene
+    ## Error handling
+    archr.proj = tryCatch({
 
-    if(calculate_p2gene == TRUE){
+            ## Run basic checks before getting to far.
+            #.run_checks()
 
-    archr.proj = peak_to_Gene(archr.proj = archr.proj,max_distance = max_distance, reduced_dims = "LSI_Combined", useMatrix = "GeneExpressionMatrix",
-                     marker.file = file.path(file.path(getOutputDirectory(archr.proj), "MarkerPeaks", groupBy), paste0(groupBy, "_annotated_markerPeaks.tsv")))
-      
-    }#end p2gene
+            ## unique_peak_calling_id
+            start_time = Sys.time()
+            unique.id = gsub(start_time, pattern = " |PDT|-|:", replacement = "")
+            print(paste0("unique.id for this run:", unique.id))
 
-    
-    ##
-    addArchRThreads(1)
-    print("Producing bigwig and fragment files")
-    generateBigWigs(archr.proj = archr.proj,
-                    groupBy = groupBy)
-    
-    ## Return the archr.proj outside of try-catch
-    archr.proj
-  }, error=function(error) {
-    message(error)
-    return(archr.proj)
-  }, finally={ 
-    print("Done!") 
-    ## Save project
-    saveArchRProject(archr.proj)
-  }
-  )
-  return(archr.proj) 
+            ## Create new directory under ArchR project for marker peak results
+            dir.create(file.path(getOutputDirectory(archr.proj), "MarkerPeaks", groupBy), showWarnings = TRUE, recursive=TRUE)
+
+            ##
+            print("Running initial ArchR setup")
+            setup(archr.genome = archr.genome,
+                  archr.threads = archr.threads)
+
+            ##
+            print("Building/checking ArchRProject")
+            archr.proj = build(arrow.file.dir = arrow.file.dir, 
+                                cell.annotation.file = cell.annotation.file, 
+                                archr.proj = archr.proj)
+
+            ##
+            if(archr.clustering == TRUE){
+                print("Performing clustering and LSI")
+                archr.proj = clusterDimReduc(archr.proj = archr.proj, 
+                                            varFeatures = varFeatures, 
+                                            resolution = resolution)
+            }
+
+            ##
+            print("Defining group coverages")
+            archr.proj = groupCoverage(archr.proj = archr.proj,
+                                    groupBy = groupBy)
+
+            ##
+            print("Calling peaks")
+            archr.proj = peakCalling(archr.proj = archr.proj,
+                                     groupBy = groupBy)
+
+            ##
+            print("Identifying marker peaks")
+            archr.proj = markerPeaks(archr.proj = archr.proj,
+                                     groupBy = groupBy, 
+                                     unique.id = unique.id)
+
+            ## Record marker table location 
+            archr.proj@projectMetadata[[paste0("loc_marker_table_", groupBy)]] = file.path(getOutputDirectory(archr.proj), "MarkerPeaks", groupBy, paste0(groupBy, "_markerPeaks.tsv"))
+
+            ##
+            if(!is.null(archr.proj@projectMetadata[[paste0(groupBy, "_markerPeaks")]])){
+                print("Loading from project")
+                marker.table = archr.proj@projectMetadata[[paste0(groupBy, "_markerPeaks")]]
+            }else{
+                print("Loading from csv")
+                marker.table = read.csv(archr.proj@projectMetadata[[paste0("loc_marker_table_", groupBy)]])
+            }
+
+            ##
+            annotatePeaks(marker.table = marker.table, 
+                        archr.proj = archr.proj, 
+                        groupBy = groupBy, 
+                        dataset = dataset,
+                        publish = publish,
+                        filename = file.path(file.path(getOutputDirectory(archr.proj), "MarkerPeaks", groupBy), paste0(groupBy, "_annotated_markerPeaks.tsv")),
+                        ucsc.user = ucsc.user,
+                        ucsc.session = ucsc.session)
+
+            ## Calculate gini index for each marker peak
+            if(calculate_gini_index == TRUE){
+              calculate_gini_index(archr.proj = archr.proj,
+                     groupBy = groupBy,filename = file.path(file.path(getOutputDirectory(archr.proj), "MarkerPeaks", groupBy), paste0(groupBy, "_annotated_markerPeaks.tsv")),
+                     max_sample_size = 500)
+            }
+
+            ## Peak to gene
+            if(calculate_peak2gene == TRUE){
+                archr.proj = peak_to_gene(archr.proj = archr.proj,
+                                            max_distance = max_distance, 
+                                            reduced_dims = "LSI_Combined", 
+                                            useMatrix = "GeneExpressionMatrix")
+            }
+
+            ##
+            print("Producing bigwig and fragment files")
+            generateBigWigs(archr.proj = archr.proj,
+                            groupBy = groupBy)
+
+            ## Return the archr.proj outside of try-catch
+            archr.proj
+        }, error=function(error) {
+            message(error)
+            return(archr.proj)
+        }, finally={ 
+            print("Done!") 
+            ## Save project
+            saveArchRProject(archr.proj)
+        }
+    )
+    return(archr.proj) 
 }
--- R/peak_to_gene.R
+++ R/peak_to_gene.R
@@ -1,72 +1,110 @@
-#'gets peak to gene links and merge with existing marker peaks'
-# archr.proj must have ar_id and ar_directory for this to work properly. Future updates will allow for counts matrices to generate gene expression matrices.
+#' Gets peak to gene links and merge with existing marker peaks
+#'
+#' @notes archr.proj must have ar_id and ar_directory for this to work properly. Future updates will allow for counts matrices to generate gene expression matrices.
+#'
 #' @param archr.proj 
 #' @param max_distance distance from the gene to search for linked peaks - default 250kb
 #' @param reduced_dims reduced dimension parameter for p2gene links ArchR function, default "LSI_Combined"
 #' @param useMatrix' matrix to use for constructing p2gene links, can be GeneExpressionMatrix (default) or GeneIntegrationMatrix
 #' @param marker.file' annotated marker peaks file path
-#' @param reduced_dims reduced dimension for p2gene links
-#' @param useMatrix matrix to use for constructing p2gene links
-#' @param maxDist maximum distance from TSS to look for correlated peaks
+#'
+#' @keywords internal
+peak_to_gene = function(archr.proj = NULL, max_distance = 250000, reduced_dims = "LSI_Combined", useMatrix = "GeneExpressionMatrix",
+                        marker.file = file.path(getOutputDirectory(archr.proj), "MarkerPeaks", groupBy), paste0(groupBy, "_annotated_markerPeaks.tsv")){
 
+    ## Ensure annotated peak file exists
+    if(!file.exists(marker.file)){print("No peak annotation file exists, please check that CERP has run properly"); return(archr.proj)}
+    
+    ## Check for GEX and LSI if not then setup.
+    if(!("GeneExpressionMatrix" %in% getAvailableMatrices(archr.proj) & 'LSI_Combined' %in% names(archr.proj@reducedDims))){
+        if("ar_id" %in% colnames(archr.proj@cellColData)& "ar_directory" %in% colnames(archr.proj@cellColData)){
+            archr.proj = add_gex_and_LSI(archr.proj = archr.proj)  
+        }else{
+            print("The ArchR project is not properly configured for peak2gene. Please refere to documentation and/or examples at: https://github.com/AllenInstitute/CERP")
+        }
+    }
 
-peak_to_Gene = function(archr.proj = NULL, max_distance = 250000, reduced_dims = "LSI_Combined", useMatrix = "GeneExpressionMatrix",
-                     marker.file = file.path(file.path(getOutputDirectory(archr.proj), "MarkerPeaks", groupBy), paste0(groupBy, "_annotated_markerPeaks.tsv"))){
-  #check for LSI if not there, run LSI 
-  if("GeneExpressionMatrix" %in% getAvailableMatrices(archr.proj) & 'LSI_Combined' %in% names(archr.proj@reducedDims)){
-    archr.proj = make_p2g_table(archr.proj = archr.proj,reducedDims = reduced_dims,useMatrix = useMatrix,maxDist = max_distance,marker.file = marker.file)
-  }else if(("GeneExpressionMatrix" %in% getAvailableMatrices(archr.proj) & 'LSI_Combined' %in% names(archr.proj@reducedDims)) == FALSE){
-    if("ar_id" %in% colnames(archr.proj@cellColData)& "ar_directory" %in% colnames(archr.proj@cellColData)){
-      archr.proj = add_gex_and_LSI(archr.proj = archr.proj)
-      archr.proj = make_p2g_table(archr.proj = archr.proj,reducedDims = reduced_dims,useMatrix = useMatrix,maxDist = max_distance,marker.file = marker.file)
-  }
-  }
-  return(archr.proj)
+    ## Compute peak to gene links
+    print("Calculating peak to gene links")
+    archr.proj = addPeak2GeneLinks(ArchRProj = archr.proj, reducedDims = reduced_dims, useMatrix = useMatrix, maxDist = max_distance)
+
+    ## Build peak to gene table
+    archr.proj = make_p2g_table(archr.proj = archr.proj,
+                                reducedDims = reduced_dims,
+                                useMatrix = useMatrix,
+                                maxDist = max_distance,
+                                marker.file = marker.file) 
+
+    return(archr.proj)
 }
 
-
-make_p2g_table = function(archr.proj = NULL,reducedDims = NULL,useMatrix = NULL,maxDist = NULL,marker.file = NULL){
-    print("calculating peak to gene links")
-    archr.proj <- addPeak2GeneLinks(ArchRProj = archr.proj,reducedDims = reduced_dims,useMatrix = useMatrix,maxDist = max_distance)
-    p2geneDF <- metadata(archr.proj@peakSet)$Peak2GeneLinks
+#' Construct peak to gene link table
+#'
+#' @param archr.proj
+#' @param marker.file
+#'
+#' @keywords internal
+make_p2g_table = function(archr.proj){
+    ## Gather peak to gene information
+    p2geneDF = metadata(archr.proj@peakSet)$Peak2GeneLinks
     p2geneDF$correlated_gene <- mcols(metadata(p2geneDF)$geneSet)$name[p2geneDF$idxRNA]
     p2geneDF$coordinates <- (metadata(p2geneDF)$peakSet %>% {paste0(seqnames(.), ":", start(.), "-", end(.))})[p2geneDF$idxATAC]
     p2geneDF$cor_FDR<-p2geneDF$FDR
     p2geneDF = as.data.table(p2geneDF)
     p2geneDF = p2geneDF[,.(coordinates,correlated_gene,Correlation,cor_FDR)]
-    marker.table = as.data.table(read.table(marker.file, sep = "\t",header = T))
-    marker.table = merge.data.table(p2geneDF,marker.table, by = "coordinates", all.x = TRUE)
-    marker.table = marker.table[!is.na(peak.id) & !is.na(Correlation),]
-    write.table(marker.table,file = marker.file, sep = "\t")
+    p2geneDF = na.omit(p2geneDF)
+    ## Load in annotated marker peak table and update
+    write.table(p2geneDF, file = file.path(getOutputDirectory(archr.proj), "MarkerPeaks", groupBy), paste0(groupBy, "_peak_to_gene.tsv"), sep = "\t")
+}
+
+#' Add gene expression and joint LSI to archr project
+#'
+#' @param archr.proj
+#'
+#' @keywords internal
+add_gex_and_LSI = function(archr.proj){
+    print("generating Gene Expression Matrix using AR directories")
+    
+    ## Add gene expression data
+    h5paths = paste0(unique(archr.proj$ar_directory), unique(archr.proj$ar_id), "/", 'outs/filtered_feature_bc_matrix.h5')
+    seRNA = import10xFeatureMatrix(input = h5paths, names = unique(archr.proj$Sample),strictMatch = F)
+    seRNA = seRNA[,colnames(seRNA) %in% archr.proj$cellNames]
+    archr.proj <- addGeneExpressionMatrix(input = archr.proj, seRNA = seRNA, force = TRUE, strictMatch = T)  ##verror unused argument strictMatch = F strictMatch
+
+    # Perform LSI on both modalities and then combine -------------------------
+    print("Calculating LSI_ATAC")
+    archr.proj <- addIterativeLSI(
+        ArchRProj = archr.proj, 
+        clusterParams = list(
+            resolution = c(0.2, 1, 2), 
+            sampleCells = 10000,
+            n.start = 10
+        ),
+        saveIterations = TRUE,
+        useMatrix = "TileMatrix", 
+        depthCol = "nFrags",
+        varFeatures = 15000,
+        name = "LSI_ATAC"
+    )
+
+    ##
+    print("Calculating LSI_RNA")
+    archr.proj <- addIterativeLSI(
+        ArchRProj = archr.proj, 
+        clusterParams = list(
+            resolution = c(0.2, 1, 2), 
+            sampleCells = 10000,
+            n.start = 10
+        ),
+        saveIterations = TRUE,
+        useMatrix = "GeneExpressionMatrix", 
+        depthCol = "Gex_nUMI",
+        varFeatures = 2500,
+        firstSelection = "variable",
+        binarize = FALSE,
+        name = "LSI_RNA"
+    )
+    print("Combining LSIs")
+    archr.proj <- addCombinedDims(archr.proj, reducedDims = c("LSI_ATAC", "LSI_RNA"), name =  "LSI_Combined")
     return(archr.proj)
 }
-
-
-add_gex_and_LSI = function(archr.proj = NULL ){
-      print("generating Gene Expression Matrix using AR directories")
-      h5paths = paste0(unique(archr.proj$ar_directory), unique(archr.proj$ar_id), "/", 'outs/filtered_feature_bc_matrix.h5')
-      seRNA = import10xFeatureMatrix(input = h5paths, names = unique(archr.proj$Sample),strictMatch = F)
-      seRNA = seRNA[,colnames(seRNA) %in% archr.proj$cellNames]
-      archr.proj <- addGeneExpressionMatrix(input = archr.proj, seRNA = seRNA, force = TRUE,strictMatch = T)  #error unused argument strictMatch = F strictMatch
-      
-      # Perform LSI on both modalities and then combine -------------------------
-      archr.proj <- addIterativeLSI(ArchRProj = archr.proj,
-                                    clusterParams = list(resolution = c(0.2, 1, 2),
-                                    sampleCells = 10000,n.start = 10),saveIterations = FALSE,
-                                    useMatrix = "TileMatrix",depthCol = "nFrags",varFeatures = 15000,name = "LSI_ATAC")
-      
-      archr.proj <- addIterativeLSI(ArchRProj = archr.proj,
-                                    clusterParams = list(resolution = c(0.2, 1, 2),
-                                    sampleCells = 10000,n.start = 10),
-                                    saveIterations = FALSE,
-                                    useMatrix = "GeneExpressionMatrix",
-                                    depthCol = "Gex_nUMI",
-                                    varFeatures = 2500,
-                                    firstSelection = "variable",
-                                    binarize = FALSE,
-                                    name = "LSI_RNA"
-      )
-      archr.proj <- addCombinedDims(archr.proj, reducedDims = c("LSI_ATAC", "LSI_RNA"), name =  "LSI_Combined")
-      return(archr.proj)
-}
-
